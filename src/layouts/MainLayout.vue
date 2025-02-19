b4823f88cd98b9fe9bab403557970a162df74fe3
Jurgo <jurgo88@gmail.com>
2025-02-19 05:28:09 +0100

    Buld fix for navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Navbar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
+import Navbar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <template>
